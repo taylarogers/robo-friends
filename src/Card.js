9ef422e214e84e794416df383795a27fdf0cf557
taylarogers <110628845+taylarogers@users.noreply.github.com>
2023-04-30 18:14:14 +0200

    Card receives input and outputs relevant info in its display

---
+++ src/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// Get the property values sent in from index.js by destructuring
+const Card = ({ name, email, id}) => {
+    return (
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            {/* Define image - ?200x200 species the size that the API must send back */}
+            <img src={`https://robohash.org/${id}?200x200`} alt="robot profile"/>
+            
+            {/* Display contact info */}
+            <div>
+                <h2> {name} </h2>
+                <p> {email} </p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
