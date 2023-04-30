f1653d17892f7f48b23e579ca7299dc2f0229f93
taylarogers <110628845+taylarogers@users.noreply.github.com>
2023-04-30 20:01:34 +0200

    Dynamically update listed robot profiles according to search value

--- src/App.js
+++ src/App.js
@@ -3,14 +3,42 @@ import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () => {
-    return (
-        <div className="tc">
-            <h1> RoboFriends </h1>
-            <SearchBox />
-            <CardList robots={robots}/>
-        </div>
-    );
+class App extends React.Component {
+    // Constructor to define state of the application
+    constructor() {
+        super()
+
+        // Robots and what is in the search field describes the state
+        this.state = {
+            robots: robots,
+            searchField: ""
+        }
+    }
+
+    // Every time input changes
+    onSearchChange = (event) => {
+        // Update state so that searchField includes what is in the search box
+        this.setState({ searchField: String(event.target.value) })
+    }
+
+    render = () => {
+        console.log(this.state.searchField)
+
+        // Filter the robots based on search criteria
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+        })
+
+        console.log(filteredRobots)
+
+        return (
+            <div className="tc">
+                <h1> RoboFriends </h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots}/>
+            </div>
+        );
+    } 
 }
 
 export default App;
